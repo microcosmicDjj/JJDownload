4fa591f85b8e82ea055f94595c88aa9497ed4a51
Mac <email@example.com>
2015-12-26 14:09:51 +0800

    Initial Commit

---
+++ JJDownload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJDownload
+//
+//  Created by Mac on 15/12/26.
+//  Copyright © 2015年 DJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JJDownload/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JJDownload
+//
+//  Created by Mac on 15/12/26.
+//  Copyright © 2015年 DJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
